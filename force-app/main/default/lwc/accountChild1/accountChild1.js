47d011296d02c90af9b6e026bedd707d9eff20eb
codingtoc <codingtoc@gmail.com>
2024-10-21 12:59:03 +0900

    Child to Parent and Parent to Child Data Communication 로직 구현

--- force-app/main/default/lwc/accountChild1/accountChild1.js
+++ force-app/main/default/lwc/accountChild1/accountChild1.js
@@ -1,3 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class AccountChild1 extends LightningElement {}
+export default class AccountChild1 extends LightningElement {
+  searchTextChild1;
+
+  handleChange(event) {
+    this.searchTextChild1 = event.target.value;
+  }
+
+  handleClick(event) {
+    const searchEvent = new CustomEvent("getsearchevent", {
+      detail: this.searchTextChild1
+    });
+    this.dispatchEvent(searchEvent);
+  }
+}
