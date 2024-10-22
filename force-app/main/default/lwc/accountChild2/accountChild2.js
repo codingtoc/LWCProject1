1edc2612a948169838ea3d99a4f4530a4b51539c
codingtoc <codingtoc@gmail.com>
2024-10-22 12:35:41 +0900

    accountChild1 to accountParent & accountParent to accountChild2 Data Communication 로직 구현

--- force-app/main/default/lwc/accountChild2/accountChild2.js
+++ force-app/main/default/lwc/accountChild2/accountChild2.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class AccountChild2 extends LightningElement {}
+export default class AccountChild2 extends LightningElement {
+  @api searchTextChild2;
+}
