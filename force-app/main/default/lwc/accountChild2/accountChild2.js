059df9c15365f7a6ee50e12ba088a72316320682
codingtoc <codingtoc@gmail.com>
2024-10-22 12:58:33 +0900

    accountChild2 컴포넌트 디자인 적용

--- force-app/main/default/lwc/accountChild2/accountChild2.js
+++ force-app/main/default/lwc/accountChild2/accountChild2.js
@@ -2,4 +2,32 @@ import { LightningElement, api } from "lwc";
 
 export default class AccountChild2 extends LightningElement {
   @api searchTextChild2;
+
+  columns = [
+    { label: "Id", fieldName: "Id" },
+    { label: "Name", fieldName: "Name" },
+    {
+      label: "Actions",
+      fieldName: "Actions",
+      type: "button",
+      typeAttributes: { label: "View Contacts", value: "view_contacts" }
+    }
+  ];
+
+  rows = [
+    { Id: "23", Name: "Parag Jambulkar", Actions: "" },
+    { Id: "30", Name: "Mayur Chavan", Actions: "" },
+    { Id: "33", Name: "Yogesh Handge", Actions: "" },
+    { Id: "40", Name: "Manish Jansari", Actions: "" }
+  ];
+
+  currentId;
+  currentName;
+
+  handleRowAction(event) {
+    if (event.detail.action.value === "view_contacts") {
+      this.currentId = event.detail.row.Id;
+      this.currentName = event.detail.row.Name;
+    }
+  }
 }
